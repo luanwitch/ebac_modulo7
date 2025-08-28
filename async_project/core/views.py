ab381c5bc9fe3b79de1ff9f0a39591d547138d13
Luan Wittchind <102259505+luanwitch@users.noreply.github.com>
2025-08-28 08:40:31 -0300

    Update views.py

--- async_project/core/views.py
+++ async_project/core/views.py
@@ -1,4 +1,12 @@
-from django.http import HttpResponse
+# async_project/views.py
+import asyncio
+from django.http import JsonResponse
 
-def post_view(request):
-    return HttpResponse("Hello word")
+async def timer_view(request):
+    # contador simples de 5 segundos
+    result = []
+    for i in range(1, 6):
+        await asyncio.sleep(1)  # espera 1 segundo sem travar o servidor
+        result.append(f"{i} segundo(s) passaram")
+
+    return JsonResponse({"contador": result})
