28683a1847a1fe3ed5dbda051e734dfb4f30165b
Luan Wittchind <102259505+luanwitch@users.noreply.github.com>
2025-08-28 08:41:39 -0300

    Update urls.py

--- async_project/async_project/urls.py
+++ async_project/async_project/urls.py
@@ -1,7 +1,6 @@
-# async_project/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("hello/", views.timer_view),  # troquei hello por timer
+    path("hello/", views.timer_view),  
 ]
