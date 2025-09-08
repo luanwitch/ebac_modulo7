65c5841758f7c06dd7b50e567799197d03492064
Luan Vieira <luanvlw23@gmail.com>
2025-09-08 10:43:56 -0300

    Mantendo apenas arquivos do exercício Módulo 7

--- core/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import post_view
-
-urlpatterns = [
-    path('post/', post_view, name='post'),
-]
--- core/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("🚀 Página inicial funcionando a partir do app core!")
